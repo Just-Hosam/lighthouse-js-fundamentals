f458c1e98355e567a448f7add338a6337ca9cb2a
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-01-09 16:59:40 +0000

    Use a while loop to repeat a chorus line

---
+++ testing.js
@@ -0,0 +1,12 @@
+var num = 12;
+var solution = 1;
+
+for (var i = num; i > 0; i--) {
+  solution *= i;
+  let test = 'this is in the block';
+  console.log(test);
+  var test2 = 'this is the second test in the block';
+}
+
+console.log(solution);
+console.log(test2);
