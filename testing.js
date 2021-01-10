58d46f1ed239093ac8aae7aff158eb1a077318bc
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-01-10 01:31:52 +0000

    just a testing file

--- testing.js
+++ testing.js
@@ -1,12 +1,6 @@
-var num = 12;
-var solution = 1;
+sayHi("Julia");
 
-for (var i = num; i > 0; i--) {
-  solution *= i;
-  let test = 'this is in the block';
-  console.log(test);
-  var test2 = 'this is the second test in the block';
+function sayHi(name) {
+  console.log(greeting + " " + name);
+  var greeting;
 }
-
-console.log(solution);
-console.log(test2);
