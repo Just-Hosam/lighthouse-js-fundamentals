80b5485531c96ce0cb236dabfa643d3107bb9a40
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-01-10 06:51:36 +0000

    just a testing file

--- testing.js
+++ testing.js
@@ -1,6 +1,17 @@
-sayHi("Julia");
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
 
-function sayHi(name) {
-  console.log(greeting + " " + name);
-  var greeting;
-}
+function chooseStations(stationArr) {
+  let goodStations = [];
+  for (let stat of stationArr) {
+    if (stat[1] >= 20 && (stat[2] === 'school' || stat[2] === 'community centre')) {
+      goodStations.push(stat[0]);
+    }
+  }
+  return goodStations;
+};
+
+console.log(chooseStations(stations));
