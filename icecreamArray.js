653a056659ed782bad8ea1cc200503d8db6529d2
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-01-10 17:16:58 +0000

    Icecream Challenge

---
+++ icecreamArray.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+const newIcecream = iceCreamFlavours.concat('root beer')
+console.log(newIcecream);
+console.log(newIcecream[0]);
+console.log(newIcecream[newIcecream.length - 1]);
+console.log(newIcecream.length);
